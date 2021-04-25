d039c3679e92b30aeda495174a168969c268a1d6
kaungmyathtaywin <kaungmyathtaywin@gmail.com>
2021-04-25 07:48:06 +0630

    Change pointer declaration format

--- week4/labs/volume.c
+++ week4/labs/volume.c
@@ -17,14 +17,14 @@ int main(int argc, char *argv[])
     }
 
     // Open files and determine scaling factor
-    FILE *input = fopen(argv[1], "r");
+    FILE* input = fopen(argv[1], "r");
     if (input == NULL)
     {
         printf("Could not open file.\n");
         return 1;
     }
 
-    FILE *output = fopen(argv[2], "w");
+    FILE* output = fopen(argv[2], "w");
     if (output == NULL)
     {
         printf("Could not open file.\n");
@@ -46,12 +46,12 @@ int main(int argc, char *argv[])
     while (fread(&buffer, sizeof(int16_t), 1, input))
     {
         // Allocate memory for the copied byte
-        int16_t *copy = malloc(sizeof(buffer));
-        
+        int16_t* copy = malloc(sizeof(buffer));
+
         // If no memory left, exit the program
         if (copy == NULL)
         {
-            printf("No memory left!");
+            printf("No memory left!\n");
             return 1;
         }
 
