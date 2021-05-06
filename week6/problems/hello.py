90c2e23b5443ee46b7bb4f0f42ad7a2983156e6d
kaungmyathtaywin <kaungmyathtaywin@gmail.com>
2021-05-06 18:09:46 +0630

    Add week six hello.py

---
+++ week6/problems/hello.py
@@ -0,0 +1,10 @@
+def main():
+    print("What's your name: ")
+
+    # Get input from the user and print it
+    name = input()
+    print(f"hello, {name}")
+
+
+if __name__ == "__main__":
+    main()
